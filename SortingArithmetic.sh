a75d2e4d0b6c94a1b89bf52cb0976e23336adca1
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-19 19:00:35 +0530

    Added: Result in Descending order.

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -11,3 +11,25 @@ DictResult[Exp2]=$((a*b+c))
 DictResult[Exp3]=$((c+a/b))
 DictResult[Exp4]=$((a%b+c))
 echo "${DictResult[@]}"
+
+for (( counter=1; counter<=4; counter++ ))
+do
+	arrayResult[$counter]=${DictResult[Exp$counter]}
+done
+
+echo "${arrayResult[@]}"
+
+for (( counter1=1; counter1<=4; counter1++ ))
+do
+	for (( counter2=1; counter2<4; counter2++ ))
+	do
+		if [[ ${arrayResult[$counter2]} -lt ${arrayResult[$(($counter2+1))]} ]]
+		then
+			temp=${arrayResult[$counter2]}
+    		arrayResult[$counter2]=${arrayResult[$((counter2+1))]}
+			arrayResult[$((counter2+1))]=$temp
+		fi
+	done
+done
+
+echo "${!arrayResult[@]} : ${arrayResult[@]} "
